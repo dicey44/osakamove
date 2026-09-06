4c963de2855e212c6b71c394658272b46c4814d9
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-09-06 19:12:50 -0400

    Make navbar wider on wide screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,35 +3,33 @@ import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container">
-                <NavLink className="navbar-brand fw-bold" to="/">
-                <i className="bi bi-house-heart-fill"> </i>
-                    OsakaMove
-                </NavLink>
+        <nav className="navbar navbar-expand-lg px-4">
+            <NavLink className="navbar-brand fw-bold" to="/">
+            <i className="bi bi-house-heart-fill"> </i>
+                OsakaMove
+            </NavLink>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <div className="navbar-nav ms-auto">
-                        <NavLink className="nav-link" to="/">
-                            ホーム
-                        </NavLink>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="navbar-nav ms-auto">
+                    <NavLink className="nav-link" to="/">
+                        ホーム
+                    </NavLink>
 
-                        <NavLink className="nav-link" to="/search">
-                            物件を探す
-                        </NavLink>
-                    </div>
+                    <NavLink className="nav-link" to="/search">
+                        物件を探す
+                    </NavLink>
                 </div>
             </div>
         </nav>
